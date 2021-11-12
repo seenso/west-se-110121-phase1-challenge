a282aa8d1d3cba4639a4c1f42ffb57fd609dc550
seenso <>
2021-11-12 09:49:43 -0900

    Missed detail about comments needing to display...

--- src/index.js
+++ src/index.js
@@ -25,12 +25,13 @@ function renderCard(data) {
     title.className = "title";
     title.innerText = data.title;
     container.append(title);
-
-  //IMAGE
-  let image = document.createElement("img");
+    
+    //IMAGE
+    let image = document.createElement("img");
     image.id = "card-image";
     image.className = "image";
     image.src = data.image;
+    toggleImage(data, title, image);
     container.append(image);
 
   //LIKES
@@ -93,11 +94,25 @@ function renderCard(data) {
 
 //BONUS DELIVERABLES
 
-//remove comment when clicked **no persistence**
+//1. remove comment when clicked **no persistence**
   //added event listener to commentForm's event listener
 function addCommentDelete(newComment) {
   newComment.addEventListener("click", (e) => {
     e.target.remove();
     console.log("newComment", e.target);
+  });
+}
+
+//2. Click title of image to toggle image displayed
+function toggleImage(data, title, image) {
+  console.log("TOGGLEIMG DATA", data);
+
+  title.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (image.src === "") {
+      image.src = data.image;
+    } else {
+      image.src = '';
+    }
   });
 }
